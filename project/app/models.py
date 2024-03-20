3a5d3c03da5afcb202ce2021a1aa64abe8c5addb
Muteeb Bhat <muteebhat7@gmail.com>
2024-03-20 14:23:27 +0530

    -models initialized

--- project/app/models.py
+++ project/app/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+class Subcategory(models.Model):
+    name = models.CharField(max_length=100)
+    category = models.ForeignKey(Category, related_name='subcategories', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    subcategory = models.ForeignKey(Subcategory, related_name='products', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
